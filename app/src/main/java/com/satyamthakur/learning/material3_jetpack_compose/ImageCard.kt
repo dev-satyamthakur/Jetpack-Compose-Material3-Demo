60854331a9dc96bc285bfa391470aee6658783db
Satyam Thakur <sat.thakur20@gmail.com>
2023-10-05 17:37:55 +0530

    Finishing up the ImageCard

--- app/src/main/java/com/satyamthakur/learning/material3_jetpack_compose/ImageCard.kt
+++ app/src/main/java/com/satyamthakur/learning/material3_jetpack_compose/ImageCard.kt
@@ -1,13 +1,35 @@
 package com.satyamthakur.learning.material3_jetpack_compose
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Share
+import androidx.compose.material3.AssistChip
+import androidx.compose.material3.AssistChipDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import kotlin.random.Random
 
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
-fun ImageCard (
+fun ImageCard(
     title: String,
     description: String,
     modifier: Modifier = Modifier
@@ -17,9 +39,67 @@ fun ImageCard (
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surfaceVariant,
 
-        ),
+            ),
         shape = MaterialTheme.shapes.large
     ) {
-
+        Image(
+            painter = rememberAsyncImagePainter(
+                model = "https://picsum.photos/seed/${Random.nextInt()}/300/200"
+            ),
+            contentDescription = null,
+            modifier = Modifier
+                .clip(MaterialTheme.shapes.large)
+                .fillMaxWidth()
+                .aspectRatio(3f / 2f)
+        )
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.titleLarge
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = description,
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            FlowRow(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                AssistChip(
+                    onClick = {},
+                    label = {
+                            Text(text = "Mark as favorite")
+                    },
+                    colors = AssistChipDefaults.assistChipColors(
+                        leadingIconContentColor = MaterialTheme.colorScheme.surfaceVariant
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = null
+                        )
+                    }
+                )
+                AssistChip(
+                    onClick = {},
+                    label = {
+                        Text(text = "Share with Others")
+                    },
+                    colors = AssistChipDefaults.assistChipColors(
+                        leadingIconContentColor = MaterialTheme.colorScheme.surfaceVariant
+                    ),
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Outlined.Share,
+                            contentDescription = null
+                        )
+                    }
+                )
+            }
+        }
     }
 }
